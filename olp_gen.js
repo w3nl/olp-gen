ec6897abeca52fdee162a904e15a366cce4fb6e1
joe-ds <dsouzaj4@uni.coventry.ac.uk>
2017-09-21 15:51:19 +0100

    Added options for speaker, reformatted page and reduced file size

--- olp_gen.js
+++ olp_gen.js
@@ -1,3 +1,12 @@
+welcome = "Welcome!" // Set a default welcome message.
+output = [];
+
+function generate(text, i) {
+    output[i] = text;
+    f_output = output.join('\n[===]\n')
+    document.getElementById("output").value = f_output;
+}
+
 function handle_osj(text) {
     osj = JSON.parse(text);
     var s = [];
@@ -9,7 +18,28 @@ function handle_osj(text) {
             s.push(o.serviceitem.header.title);
         };
     }
-    document.getElementById("output").value = s.join("\n\n");//<br/><br/>");
+    generate(s.join("\n\n"), 2);
+}
+
+function speaker(evt) {
+    s = evt.target.value;
+    if (s) {
+        generate(s, 1);
+    };
+}
+
+function scripture(evt) {
+    book = document.getElementById("book").value;
+    chap = document.getElementById("chapter").value.toString();
+    verse = document.getElementById("verse").value.toString();
+    speaker = document.getElementById("speaker").value;
+    if (book == "unknown") {
+        text = null;
+        generate(speaker, 1);
+    } else { 
+        text = book + " " + chap + ":" + verse;
+        generate(speaker + "\n\n" + text, 1);
+    };
 }
 
 function handleFile(evt) {
@@ -48,12 +78,24 @@ function handleFile(evt) {
           // onerror callback
           document.getElementById("output").value = 'Unexpected error';
         });
-    }
+    };
 
 window.onload = function() {
     if (window.File && window.Blob) {
-        var x = document.getElementById("file");
-        x.addEventListener("change", handleFile, false);
+        output = [welcome, document.getElementById("speaker").value]
+        var x = document.getElementById("file").addEventListener("change", handleFile, false);
+        var y = document.getElementById("speaker").addEventListener("change", speaker, false);
+        var z = document.getElementById("scripture").addEventListener("change", scripture, false);
+        document.getElementById("copy").addEventListener("click", function(evt) {
+            var x = document.getElementById("output");
+            x.select();
+
+            try {
+                document.execCommand('copy');
+            } catch (err) {
+                evt.target.disabled;
+            };
+        });
     } else {
         document.getElementById("output").value = 'APIs unsupported';
     };
